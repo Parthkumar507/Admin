96c5e07b0feadb4df37bedef88338c90db9aba05
Parthkumar507 <parthkumar281202@gmail.com>
2023-11-29 11:58:48 +0530

    Password Hashing

--- Backend/index.js
+++ Backend/index.js
@@ -3,9 +3,9 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const connectDB=require('./model/permission.js')
-const User=require('./model/user.js')
-
+const connectDB = require("./model/permission.js");
+const User = require("./model/user.js");
+const bcrypt=require('bcryptjs')
 
 /* CONFIGURATION */
 dotenv.config();
@@ -18,41 +18,44 @@ app.use(cors());
 //Middleware for parsing Json
 app.use(express.json());
 
-
-
 //Registration
-app.post('/register',async(req,res) => {
-  try{
-      const {name,password,email,role} = req.body;
-      console.log('```````````````````````````')
-      console.log(req.body)
-      const user = new User({name,password,email,role});
-      await user.save();
-      res.status(201).json({message:'Registration Successful'});
+app.post("/register", async (req, res) => {
+  try {
+    const { name, password, email, role } = req.body;
+    // console.log('```````````````````````````')
+    // console.log(req.body)
+    const user = new User({ name, password, email, role });
+    await user.save();
+    res.status(201).json({ message: "Registration Successful" });
+  } catch (error) {
+    res.status(500).json({ error: "Registration failed" });
   }
-  catch(error){
-      res.status(500).json({error:'Registration failed'});
-  }
-})
+});
 
-//Login
-app.post('/login',async(req,res)=>{
-  try{
-      const{email,password}=req.body;
-      const UserinDB = await User.findOne({email});
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userInDB = await User.findOne({ email });
 
-      if(!UserinDB){
-        return res.status(401).json({error:"Incorrect email Id or Password"});
-      }
-      if(UserinDB.password !== password){
-        return res.status(401).json({error:'Invalid username or password'});
+    if (!userInDB) {
+      return res.status(401).json({ error: "Incorrect email or password" });
     }
-    res.status(200).json({ message: 'Login successful' })
+
+    const passwordCheck = await bcrypt.compare(password, userInDB.password);
+
+    if (passwordCheck) {
+      // console.log("Authentication successful");
+      return res.status(200).json({ message: "Login successful" });
+    } else {
+      console.log("Authentication failed");
+      return res.status(401).json({ error: "Incorrect email or password" });
+    }
+  } catch (error) {
+
+    console.error("Error during login:", error);
+    return res.status(500).json({ error: "Login failed" });
   }
-  catch(error){
-    return res.status(500).json({error:"Login failed"});
-  }
-})
+});
 
 
 // MONGOOSE SETUP
@@ -60,9 +63,8 @@ const PORT = process.env.PORT || 8000;
 connectDB();
 
 // use express router
-app.use('/',require('./router/index.js'))
+app.use("/", require("./router/index.js"));
 
-
-  app.listen(PORT, () => {
-    console.log(`Server Port: ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server Port: ${PORT}`);
 });
--- Backend/model/user.js
+++ Backend/model/user.js
@@ -1,4 +1,7 @@
 const mongoose=require('mongoose')
+const bcrypt=require('bcryptjs')
+
+
 // Define the email validation function
 function validateEmail(email) {
     // Your email validation logic here
@@ -32,9 +35,31 @@ const UserSchema= mongoose.Schema({
     },
     role:{
         type:String,
-        rquire:true,
+        require:true,
     }
 });
+
+UserSchema.pre('save', function (next) {
+  const user = this;
+
+  // Only hash the password if it has been modified or is new
+  if (!user.isModified('password')) return next();
+
+  // Generate a salt
+  bcrypt.genSalt(10, function (err, salt) {
+      if (err) return next(err);
+
+      // Hash the password along with the new salt
+      bcrypt.hash(user.password, salt, function (err, hash) {
+          if (err) return next(err);
+
+          // Override the plaintext password with the hashed one
+          user.password = hash;
+          next();
+      });
+  });
+});
+
 
 const User=mongoose.model('User',UserSchema);
 
