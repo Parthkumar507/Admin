281d85f2fa5bd2529620e9fc1d49cc06e84380b1
Parthkumar507 <parthkumar281202@gmail.com>
2023-11-30 01:51:14 +0530

    Code distribution and improve readibility

--- Backend/index.js
+++ Backend/index.js
@@ -7,8 +7,7 @@ const connectDB = require("./model/permission.js");
 const User = require("./model/user.js");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-//Used for session cookie + authentication
-const session =require('express-session')
+const user_routes=require('./router/user_routes.js')
 
 /* CONFIGURATION */
 dotenv.config();
@@ -24,60 +23,72 @@ app.use(cors({
 //Middleware for parsing Json
 app.use(express.json());
 
-//Registration
-app.post("/register", async (req, res) => {
-  try {
-    const { name, password, email, role } = req.body;
-    // console.log('```````````````````````````')
-    // console.log(req.body)
-    const user = new User({ name, password, email, role });
-    await user.save();
-    res.status(201).json({ message: "Registration Successful" });
-  } catch (error) {
-    res.status(500).json({ error: "Registration failed" });
-  }
-});
 
-app.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const userInDB = await User.findOne({ email });
-
-    if (!userInDB) {
-      return res.status(401).json({ error: "Incorrect email or password" });
-    }
-    const passwordCheck = await bcrypt.compare(password, userInDB.password);
-
-    if (passwordCheck) {
-      console.log("Authentication successful");
-      const token=await userInDB.getAuthenticateToken();
-      console.log(token);
-
-      res.cookie("tokenCookie",token,{
-        expires:new Date(Date.now()+2500000000),
-        httpOnly:true,
-        sameSite: 'None', 
-
-      })  
-
-      return res.status(200).json({ message: "Login successful" });
-    } else {
-      console.log("Authentication failed");
-      return res.status(401).json({ error: "Incorrect email or password" });
-    }
-  } catch (error) {
-    console.error("Error during login:", error);
-    return res.status(500).json({ error: "Login failed" });
-  }
-});
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 8000;
 connectDB();
 
 // use express router
-app.use("/", require("./router/index.js"));
+app.use("/", user_routes);
 
 app.listen(PORT, () => {
   console.log(`Server Port: ${PORT}`);
 });
+
+
+// //Registration
+// app.post("/register", async (req, res) => {
+//   try {
+//     const { name, password, email, role } = req.body;
+//     // console.log('```````````````````````````')
+//     // console.log(req.body)
+//     const user = new User({ name, password, email, role });
+//     await user.save();
+//     res.status(201).json({ message: "Registration Successful" });
+//   } catch (error) {
+//     res.status(500).json({ error: "Registration failed" });
+//   }
+// });
+
+
+
+
+
+
+// `````used controller```
+
+// app.post("/login", async (req, res) => {
+//   res.cookie("test","1 okayyyyy",{
+//     expires:new Date(Date.now()+2500000000),
+//     })
+//   try {
+//     const { email, password } = req.body;
+//     const userInDB = await User.findOne({ email });
+
+//     if (!userInDB) {
+//       return res.status(401).json({ error: "Incorrect email or password" });
+//     }
+//     const passwordCheck = await bcrypt.compare(password, userInDB.password);
+
+//     if (passwordCheck) {
+//       console.log("Authentication successful");
+//       const token=await userInDB.getAuthenticateToken();
+//       // console.log(token);
+
+//       res.cookie("tokenCookie",token,{
+//         expires:new Date(Date.now()+2500000000),
+//         httpOnly:true,
+//         // path:"http://localhost:3000/"
+//       })  
+
+//       return res.status(200).json({ message: "Login successful" });
+//     } else {
+//       console.log("Authentication failed");
+//       return res.status(401).json({ error: "Incorrect email or password" });
+//     }
+//   } catch (error) {
+//     console.error("Error during login:", error);
+//     return res.status(500).json({ error: "Login failed" });
+//   }
+// });
