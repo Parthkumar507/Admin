8f7be93ff9c6fea77a6f82fa299912a39961a63f
Parthkumar507 <parthkumar281202@gmail.com>
2023-12-01 10:01:00 +0530

    Update HomePage to handle Authentication

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,20 +1,74 @@
-// Sidebar.js
-
-import React from 'react';
-import { Link } from 'react-router-dom'; // If you're using React Router for navigation
-import '../style/Home_page.css'; // Import the styles
-
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../style/Home_page.css';
+import '../Auth/userAuth'
 
 const Sidebar = () => {
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const { authenticated } = useAuth();
+
+
+  // const callHomePage = async () => {
+  //   try {
+  //     const resp = await fetch('/', {
+  //       method: 'GET',
+  //       headers: {
+  //         Accept: 'application/json',
+  //         'Content-Type': 'application/json',
+  //       },
+  //       credentials: "include",
+  //     });
+  //     console.log("0")
+  //     // if (resp.ok) 
+  //     {
+  //       const data = await resp.json();
+  //       console.log("1")
+  //       console.log(data)
+
+        
+
+  //       setUserData(data.user);
+  //     }
+  //     //  else {
+  //     //   setError('Failed to fetch user data');
+  //     // }
+  //   } catch (err) {
+  //     setError('An error occurred while fetching user data');
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   callHomePage();
+  // }, []);
+
   return (
+   
     <div className="sidebar">
+     {/* {authenticated && ( */}
       <h2>Dashboard</h2>
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/contact">Contact</Link></li>
       </ul>
+
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+
+      {userData && (
+        <div>
+          <h3>User Data</h3>
+          <p>Username: {userData.username}</p>
+          {/* Display other user data properties as needed */}
+        </div>
+      )}
+      {/* )} */}
     </div>
+    
   );
 };
 
