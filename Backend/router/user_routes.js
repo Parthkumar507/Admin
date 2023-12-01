d43a3025542f7b772248d349658f45d008f5e8aa
Parthkumar507 <parthkumar281202@gmail.com>
2023-12-01 09:59:38 +0530

    Add verifyToken middleware for verification

--- Backend/router/user_routes.js
+++ Backend/router/user_routes.js
@@ -3,13 +3,29 @@ const router = express.Router();
 
 const {
    login,
-   register
+   register,
+   verifyToken,
+   getUser
   } = require("../controllers/user_controller");
 
 console.log('router loaded');
 
 router.post('/login',login);
 router.post('/register',register);
+
+router.get('/',verifyToken,async (req,res)=>{
+  console.log("Hellooooooo this is home page")
+  try {
+     await getUser(req, res);
+    // res.json(rootUser);
+  } catch (error) {
+    // Handle any errors that occurred in getUser
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+  // If getUser succeeded, you can continue with your logic here
+  res.json({ message: "Home page data" });
+})
 
 
 module.exports=router;
