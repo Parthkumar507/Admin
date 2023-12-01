0ce73e8119fb6b2c9502a48108c991b11eadbee1
Parthkumar507 <parthkumar281202@gmail.com>
2023-12-01 19:49:57 +0530

    Add header file

---
+++ client/src/components/Header.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { AppBar, Box, Tab, Tabs, Toolbar, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
+
+const Header = () => {
+  const [value, setValue] = useState();
+
+  return (
+    <div>
+      <AppBar position="sticky">
+        <Toolbar>
+          <Typography variant="h3">JobPortal</Typography>
+          <Box sx={{ marginLeft: "auto" }}>
+            <Tabs
+                indicatorColor="secondary"
+                onChange={(e, val) => setValue(val)}
+              value={value}
+              textColor="inherit"
+            >
+              <Tab to="/login" LinkComponent={Link} label="login" />
+              <Tab to="/welcome" LinkComponent={Link} label="Welcome Page" />
+              {/* <Tab to="/logout" LinkComponent={Link} label="Logout" /> */}
+            </Tabs>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Header;
