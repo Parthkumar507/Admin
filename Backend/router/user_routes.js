1f301d0371ed7f5b99f931c7f4575c60dd9eae63
Parthkumar507 <parthkumar281202@gmail.com>
2023-12-01 13:44:28 +0530

    Remove the function from route

--- Backend/router/user_routes.js
+++ Backend/router/user_routes.js
@@ -13,19 +13,7 @@ console.log('router loaded');
 router.post('/login',login);
 router.post('/register',register);
 
-router.get('/',verifyToken,async (req,res)=>{
-  console.log("Hellooooooo this is home page")
-  try {
-     await getUser(req, res);
-    // res.json(rootUser);
-  } catch (error) {
-    // Handle any errors that occurred in getUser
-    console.error(error);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-  // If getUser succeeded, you can continue with your logic here
-  res.json({ message: "Home page data" });
-})
+router.get('/home',verifyToken,getUser)
 
 
 module.exports=router;
