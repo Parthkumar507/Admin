0cd3bce1d70f985ba5040fe7f811b933ffe0b441
Parthkumar507 <parthkumar281202@gmail.com>
2023-12-02 19:24:33 +0530

    Handle Permission route to add new permission

--- Backend/model/permission.js
+++ Backend/model/permission.js
@@ -1,13 +1,13 @@
-const mongoose=require('mongoose');
-const connectDB=async()=>
-{
-    try{
-        await mongoose.connect('mongodb://127.0.0.1:27017/registerUserDB')
-        console.log('Connected to MongoDB');
-    }
-    catch(error){
-        console.log(`MongoDb did not connect , error: ${error}`)
-    } 
-}
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-module.exports =connectDB;
+const PermissionSchema = new Schema({
+  
+    role:{
+        type: String,
+        required: true
+    },
+    permission : []
+  });
+
+  module.exports = mongoose.model('permission', PermissionSchema);
