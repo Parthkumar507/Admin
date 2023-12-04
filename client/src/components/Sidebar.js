dc209df4d8d76ccb829a58cf21c68ef10392473e
Parthkumar507 <parthkumar281202@gmail.com>
2023-12-04 15:30:53 +0530

    Add DropDown and add permission check

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,62 +1,81 @@
 // Sidebar.js
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
 import "../style/sidebar.css";
 import { Link } from "react-router-dom";
+import axios from 'axios';
 
-const Sidebar = () => {
+const Sidebar = ({user}) => {
+  const dropdowns = [
+    { id: 1, label: "Job", items: ["Post", "Update", "Delete"], permission: "job" },
+    { id: 2, label: "Resume", items: ["Post", "Update", "Delete"], permission: "resume" },
+    { id: 3, label: "Settings", items: ["Team/Users", "Payment Settings", "Roles and Permission", "Email Setting"], permission: "settings" },
+    // Add more dropdowns as needed
+  ];
+
   const [openDropdown, setOpenDropdown] = useState(null);
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [userPermissions, setUserPermissions] = useState([]);
 
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
-  };
+  useEffect(() => {
+    const fetchUserPermissions = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8000/api/fetchpermission/${user._id}`);
+        setUserPermissions(response.data.permissions);
+      } catch (error) {
+        console.error('Error fetching user permissions:', error);
+      }
+    };
 
-  const handleItemClick = (item) => {
-    // Do something when an item is clicked
-    console.log(`Selected: ${item}`);
-    // You can also close the dropdown after an item is clicked if needed
-    setIsDropdownOpen(false);
-  };
-
-  const handleDropdownToggle = (field) => {
-    if (openDropdown === field) {
-      setOpenDropdown(null);
-    } else {
-      setOpenDropdown(field);
+    if (user) {
+      fetchUserPermissions();
     }
+  }, [user]);
+
+  const hasPermission = (permission) => {
+    // console.log("userPermissions : ", userPermissions)
+    // console.log("permission : ", permission)
+    // console.log("userPermissions.includes(permission)",userPermissions.includes(permission));
+    return userPermissions.includes(permission);
+  };
+
+
+  const toggleDropdown = (dropdownId) => {
+    setOpenDropdown(openDropdown === dropdownId ? null : dropdownId);
+  };
+
+  const handleItemClick = (dropdownId, item) => {
+    // Do something when an item is clicked
+    console.log(`Selected from ${dropdownId}: ${item}`);
+    // Close the dropdown after an item is clicked
+    setOpenDropdown(null);
   };
 
   return (
     <div className="sidebar">
       <>
-        <h2>Dashboard</h2>
+        <h2>Welcome {user && <h1>{user.name}</h1>}</h2>
         <ul>
-          <li>
-            <button onClick={toggleDropdown}>Report ▼ </button>
-            {isDropdownOpen && (
-              <ul className="dropdown-content">
-                <li onClick={() => handleItemClick("Item 1")}>Item 1</li>
-                <li onClick={() => handleItemClick("Item 2")}>Item 2</li>
-                <li onClick={() => handleItemClick("Item 3")}>Item 3</li>
-              </ul>
-              
-            )}
-            {/* <span onClick={() => handleDropdownToggle("dashboard")}></span> */}
-
-            </li>
-            <li>
-            <button onClick={toggleDropdown}>Students ▼</button>
-            {isDropdownOpen && (
-              <ul className="dropdown-content">
-                <li onClick={() => handleItemClick("Item 1")}>Item 1</li>
-                <li onClick={() => handleItemClick("Item 2")}>Item 2</li>
-                <li onClick={() => handleItemClick("Item 3")}>Item 3</li>
-              </ul>
-            )}
-            {/* <span onClick={() => handleDropdownToggle("dashboard")}>▼</span> */}
-          </li>
-
+          {dropdowns.map((dropdown) => (
+            hasPermission(dropdown.permission) && (
+            <li key={dropdown.id}>
+              <button onClick={() => toggleDropdown(dropdown.id)}>
+                {dropdown.label} {openDropdown === dropdown.id ? "▲" : "▼"}
+              </button>
+              {openDropdown === dropdown.id && (
+                <ul className="dropdown-content">
+                  {dropdown.items.map((item, index) => (
+                    
+                      <li
+                      key={index}
+                      onClick={() => handleItemClick(dropdown.id, item)}
+                    >
+                    {item}
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </li>
+            )
+          ))}
           <li>
             <Link to="/logout">Logout</Link>
           </li>
