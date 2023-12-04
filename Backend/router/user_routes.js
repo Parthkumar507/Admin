1c8cc466721c62b154be04284a16d71e49d6af29
Parthkumar507 <parthkumar281202@gmail.com>
2023-12-04 15:23:49 +0530

    Add API routes to handle Permission task

--- Backend/router/user_routes.js
+++ Backend/router/user_routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
-const Permission = require('../model/Permission')
+
 
 
 const {
@@ -12,6 +12,11 @@ const {
   //  authMiddleware
   } = require("../controllers/user_controller");
 
+const {
+    addPermission,
+    fetchPermission
+}=require('../controllers/api_controller')
+
 console.log('router loaded');
 
 // router.post('/login',authMiddleware,login);
@@ -21,30 +26,12 @@ router.post('/register',register);
 router.get('/welcome',verifyToken,getUser)
 
 // ROUTE 1: Add a new Note using: POST "/api/permission/addpermission". Login required
-router.post('/api/addpermission',  [
-  body('role', 'Enter a valid role').isLength({ min: 3 }),
- 
-   ], async (req, res) => {
-      try {
-          const { role ,permission } = req.body;
-          // If there are errors, return Bad request and the errors
-          console.log(req.body)
-          const errors = validationResult(req);
-          if (!errors.isEmpty()) {
-              return res.status(400).json({ errors: errors.array() });
-          }
-          const permissionData = new Permission({
-              role ,permission
-          })
-          const savedNote = await permissionData.save()
-          res.json(savedNote)
-          console.log(savedNote)
+router.post('/api/addpermission', verifyToken,addPermission);
 
-      } catch (error) {
-          console.error(error.message);
-          res.status(500).send("Internal Server Error");
-      }
-  })
+// ROUTE 2: Add a new Note using: GET "/api/permission/fetchpermission". Login required
+router.get('/api/fetchpermission/:userId',verifyToken, fetchPermission);
 
 
-module.exports=router;
+module.exports=router
+
+  
