27ddc8e6e8e4339f88cf31e7ed51bea89631e347
Parthkumar507 <parthkumar281202@gmail.com>
2023-12-11 17:49:20 +0530

    Add userLogout route

--- Backend/router/user_routes.js
+++ Backend/router/user_routes.js
@@ -10,12 +10,13 @@ const {
    verifyToken,
    getUser,
   //  authMiddleware
+  userLogout,
   } = require("../controllers/user_controller");
 
 const {
     addPermission,
     fetchPermission,
-    getRoles
+    getRoles,
 }=require('../controllers/api_controller')
 
 console.log('router loaded');
@@ -26,14 +27,18 @@ router.post('/register',register);
 
 router.get('/welcome',verifyToken,getUser)
 
+router.post('/logout',verifyToken,userLogout);
+
 // ROUTE 1: Add a new Note using: POST "/api/permission/addpermission". Login required
 router.post('/api/addpermission', verifyToken,addPermission);
 
 // ROUTE 2: Add a new Note using: GET "/api/permission/fetchpermission". Login required
-router.get('/api/fetchpermission/:userId',verifyToken, fetchPermission);
+router.get('/api/fetchpermission/',verifyToken, fetchPermission);
 
 // ROUTE 3: Add a new Note using: GET "/api/getRole". Login required
 router.get('/api/getRole',verifyToken,getRoles );
+
+
 
 
 
