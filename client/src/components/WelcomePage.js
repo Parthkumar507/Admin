0c0850494a21a1a6f8f0c828cdc23fee5982b3f3
Parthkumar507 <parthkumar281202@gmail.com>
2023-12-11 17:57:16 +0530

    Commenting all , as they are not useful

--- client/src/components/WelcomePage.js
+++ client/src/components/WelcomePage.js
@@ -4,41 +4,41 @@ import Sidebar from "../components/Sidebar"; // Import your Sidebar component
 axios.defaults.withCredentials = true;
 
 const WelcomePage = () => {
-  const [user, setUser] = useState();
+  // const [user, setUser] = useState();
 
-  const sendRequest = async () => {
-    try {
-      const resp = await axios.get("http://localhost:8000/welcome", {
-        withCredentials: true,
-      });
+  // const sendRequest = async () => {
+  //   try {
+  //     const resp = await axios.get("http://localhost:8000/welcome", {
+  //       withCredentials: true,
+  //     });
 
-      return resp.data;
-    } catch (error) {
-      console.log(error);
+  //     return resp.data;
+  //   } catch (error) {
+  //     console.log(error);
 
-      // Assuming your server sends a specific status code for unauthorized access
-      if (error.response) {
-        // Redirect to login page
-        window.location.href = "http://localhost:3000/login";
-      }
+  //     // Assuming your server sends a specific status code for unauthorized access
+  //     if (error.response) {
+  //       // Redirect to login page
+  //       window.location.href = "http://localhost:3000/login";
+  //     }
 
-      // Handle other error cases if needed
-      throw error;
-    }
-  };
+  //     // Handle other error cases if needed
+  //     throw error;
+  //   }
+  // };
 
-  useEffect(() => {
-    sendRequest()
-      .then((data) => setUser(data.user))
-      .catch((error) => {
-        // Handle errors if necessary
-      });
-  }, []);
+  // useEffect(() => {
+  //   sendRequest()
+  //     .then((data) => setUser(data.user))
+  //     .catch((error) => {
+  //       // Handle errors if necessary
+  //     });
+  // }, []);
 
   return (
     <div>
-      <Sidebar user={user} /> {/* Include the Sidebar component here */}
-      <div>Welcome {user && <h1>{user.name}</h1>}</div>
+      {/* <Sidebar user={user} />  */}
+       {/* <div>Welcome {user && <h1>{user.name}</h1>}</div> */}
     </div>
   );
 };
