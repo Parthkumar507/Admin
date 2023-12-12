53232645805cbd0a7b254f00357e75166a39d9e9
Parthkumar507 <parthkumar281202@gmail.com>
2023-12-12 18:04:56 +0530

    Migration of DB from local to cloud

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,8 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const connectDB = require("./model/ConnectMongoDB");
+// const connectDB = require("./model/ConnectMongoDB");
+// const connectDBcloud = require("./model/ConnectMongoCloud.js");
 const User = require("./model/user.js");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
@@ -30,7 +31,20 @@ app.use(express.json());
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 8000;
-connectDB();
+
+//Local Storage MongoDB Method
+// connectDB();
+
+const connectionString = process.env.MONGO_URL;
+// const connectionString = `mongodb+srv://Parth:${passWord}@cluster0.gzcbjqa.mongodb.net/?retryWrites=true&w=majority`
+// Connect to MongoDB
+mongoose.connect(connectionString)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
 
 // use express router
 app.use("/", user_routes);
@@ -39,59 +53,3 @@ app.listen(PORT, () => {
   console.log(`Server Port: ${PORT}`);
 });
 
-
-// //Registration
-// app.post("/register", async (req, res) => {
-//   try {
-//     const { name, password, email, role } = req.body;
-//     // console.log('```````````````````````````')
-//     // console.log(req.body)
-//     const user = new User({ name, password, email, role });
-//     await user.save();
-//     res.status(201).json({ message: "Registration Successful" });
-//   } catch (error) {
-//     res.status(500).json({ error: "Registration failed" });
-//   }
-// });
-
-
-
-
-
-
-// `````used controller```
-
-// app.post("/login", async (req, res) => {
-//   res.cookie("test","1 okayyyyy",{
-//     expires:new Date(Date.now()+2500000000),
-//     })
-//   try {
-//     const { email, password } = req.body;
-//     const userInDB = await User.findOne({ email });
-
-//     if (!userInDB) {
-//       return res.status(401).json({ error: "Incorrect email or password" });
-//     }
-//     const passwordCheck = await bcrypt.compare(password, userInDB.password);
-
-//     if (passwordCheck) {
-//       console.log("Authentication successful");
-//       const token=await userInDB.getAuthenticateToken();
-//       // console.log(token);
-
-//       res.cookie("tokenCookie",token,{
-//         expires:new Date(Date.now()+2500000000),
-//         httpOnly:true,
-//         // path:"http://localhost:3000/"
-//       })  
-
-//       return res.status(200).json({ message: "Login successful" });
-//     } else {
-//       console.log("Authentication failed");
-//       return res.status(401).json({ error: "Incorrect email or password" });
-//     }
-//   } catch (error) {
-//     console.error("Error during login:", error);
-//     return res.status(500).json({ error: "Login failed" });
-//   }
-// });
